ee52c79d08793dd6bf2d7b1493c82282f2688f84
devendra-dev-ui <mmylapal@student.gitam.edu>
2026-02-05 15:02:27 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    if n == 0:  # Base case
+        return 1
+    else:       # Recursive case
+        return n * factorial(n - 1)
+
+print(factorial(4))
+
+#output
+
+24
+
+=== Code Execution Successful ===
