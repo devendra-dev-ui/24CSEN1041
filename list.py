efe432959acd2b9c4eac13b6986f0e47d33a5c85
devendra-dev-ui <mmylapal@student.gitam.edu>
2026-02-09 14:50:15 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,17 @@
+#list
+
+list_1 = ['2025095919' , "devendra", "5", "4", "7", "apple", "2.00"]
+print(list_1)
+print(type(list_1))
+print(list_1[-4])
+print(len(list_1))
+print(len(list_1[4]))
+
+#output
+['2025095919', 'devendra', '5', '4', '7', 'apple', '2.00']
+<class 'list'>
+4
+7
+1
+
+=== Code Execution Successful ===
